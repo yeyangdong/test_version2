b2c153b4a0f605dff4cee65702bb7fe0c93fe6fd
Vachel <17606701497@163.com>
2023-04-12 12:17:08 +0800

    批量注册脚本在-2u3y4文件中

--- 草稿/2u3y4.py
+++ 草稿/2u3y4.py
@@ -37,23 +37,88 @@
 #         b = nums.index(i)
 #         print(b)
 
+#
+# strs = ["flower", "flow", "flight"]
+#
+#
+# class Solution(object):
+#     def longestCommonPrefix(self, strs):
+#         n = len(strs)
+#         if n == 0:
+#             return None
+#         for i in range(len(strs[0])):
+#             first = strs[0][i]
+#             for j in range(1, n):
+#                 if i == len(strs[j]) or first != strs[j][i]:
+#                     return strs[0][0:i]
+#         return strs[0]
+#
+#
+#
+# test = Solution()
+# test.longestCommonPrefix(["flower", "flow", "flight"])
 
-strs = ["flower", "flow", "flight"]
+
+#
+# class Solution:
+#     def isIsomorphic1(self, s: str, t: str) -> bool:
+#         a , b= zip(*zip(s, t))
+#         print(a,b)
+#
+#
+#
+#
+# pasew = Solution()
+#
+# pasew.isIsomorphic1
+
+import requests
+import random
+
+# {code: 121212, password: yyd1998713, lang: zh - CN, money_unit: CNY, mobile: 1008659, mobile_prefix: +86}
+def register():
+    url = 'http://test-api.ddpurse.com:6001/v1/users'
+    method = 'post'
+    headers = {
+        "Authorization":""
+    }
+
+    #生成手机号
+    phone_title = ['135', '143']
+    phone_title_1 = random.choice(phone_title)
+    phone_bottom = "".join(random.choice("0123456789") for i in range(8))
+    all_phone = phone_title_1 + phone_bottom
+
+    #参数
+    form_data = {
+        "mobile_prefix":"+86",
+        "mobile":all_phone,
+        "password":"yyd1998713",
+        "code":"121212",
+        "money":"CNY",
+        "lang":"zh-CN"
+    }
+
+    # 发起请求
+    response = requests.request(method=method.upper(),url=url,params=form_data,headers=headers)
+
+    code = response.json()["code"]
+    if code == "200":
+        print("success")
+    else:
+        print("false")
+
+    return response
 
 
-class Solution(object):
-    def longestCommonPrefix(self, strs):
-        n = len(strs)
-        if n == 0:
-            return None
-        for i in range(len(strs[0])):
-            first = strs[0][i]
-            for j in range(1, n):
-                if i == len(strs[j]) or first != strs[j][i]:
-                    return strs[0][0:i]
-        return strs[0]
+if __name__ == '__main__':
+   i = 1
+   while i < 10:
+       print(f"第{i}个注册")
+       i = i+ 1
+       medo = register()
 
 
 
-test = Solution()
-test.longestCommonPrefix(["flower", "flow", "flight"])
+
+
